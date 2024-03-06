f89966d723e3bfaafd6c10a28c2824e1cfeece73
Mannat Jaiswal <mannatjaiswal03@gmail.com>
2024-03-06 20:45:05 +0530

    Made the site responsive

--- app.js
+++ app.js
@@ -80,15 +80,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.get("/demouser", async (req, res) => {
-//   let fakeUser = new User({
-//     email: "student@gmail.com",
-//     username: "student@vit",
-//   });
-//   let registeredUser = await User.register(fakeUser, "helloworld");
-//   res.send(registeredUser);
-// });
-
 // routes
 app.get(
   "/",
